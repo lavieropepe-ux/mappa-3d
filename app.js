2b9c7d042103217adb5884841104efc54e88ab22
LP <laviero.pepe@unibas.it>
2026-09-17 15:11:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ const customLayer = {
     },
     render: function (gl, matrix) {
         const rotationX = new THREE.Matrix4().makeRotationAxis(
-            new THREE.Vector3(1, 0, 0),
+            new THREE.Vector3(0, 1, 0),
             modelTransform.rotateX
         );
         const rotationY = new THREE.Matrix4().makeRotationAxis(
